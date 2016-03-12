9081808f48d3a8419ccb9da39b6090c897b6fed9
Zacoru <p.rand.gallagher@gmail.com>
2016-03-12 07:39:58 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,2 +1,2 @@
-setInterval(function(){}, 9999); //Dummy SetInterval
+setInterval(function(){}, 9999); //Dummy SetInterval (Gets cleared during netflix's load events)
 setInterval(function(){try{document.getElementsByClassName("button continue-playing")[0].click()}catch(e){}}, 33);
