07aa88a27b6258fd9cb3f411dcda48e15cbf7639
PrescottGallagher <p.rand.gallagher@gmail.com>
2018-03-08 03:59:43 -0500

    Update background.js
    Updated to keep up with Netflix's patch.

--- background.js
+++ background.js
@@ -1,2 +1,2 @@
 setInterval(function(){}, 9999); //Dummy SetInterval (Gets cleared during netflix's load events)
-setInterval(function(){try{document.getElementsByClassName("button continue-playing")[0].click()}catch(e){}}, 33);
+setInterval(function(){try{document.getElementsByClassName("interrupter-actions")[0].getElementsByClassName("nf-flat-button")[0].click()}catch(e){}}, 33);
